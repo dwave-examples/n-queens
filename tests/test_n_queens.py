2c5d28a8c066d3c423b960a523e6b7d3559c4eb7
Heidi Tong <htong@dwavesys.com>
2021-04-30 15:29:43 -0700

    Skip integration test unless on linux with latest python

--- tests/test_n_queens.py
+++ tests/test_n_queens.py
@@ -23,8 +23,9 @@ from n_queens import *
 
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-class TestSmoke(unittest.TestCase):
-    def test_smoke(self):
+class TestNQueens(unittest.TestCase):
+    @unittest.skipIf(os.getenv('SKIP_INT_TESTS'), "Skipping integration test.")
+    def test_integration(self):
         # check that nothing crashes
         demo_file = os.path.join(project_dir, 'n_queens.py')
         p = Popen([sys.executable, demo_file],
@@ -43,25 +44,30 @@ class TestSmoke(unittest.TestCase):
         self.assertTrue(os.path.isfile(image_name))
         os.remove(image_name)
 
+    def test_invalid_solutions(self):
+        n = 4
+
+        # 2 queens in a column
+        solution = [{0, 10, 7}, 
+                    {0, 9, 6, 17}, 
+                    {16, 2, 12, 7}, 
+                    {11, 13, 3, 5}]
+        self.assertFalse(is_valid_solution(n, solution))
+
+        # 0 queens in a row
+        solution = [{0, 9, 6, 17}, 
+                    {1, 7, 11, 17}, 
+                    {16, 2, 12, 7}, 
+                    {11, 13, 3, 5}]
+        self.assertFalse(is_valid_solution(n, solution))
+
     def test_4_queens(self):
         sampler = ExactSolver()
 
         n = 4
         solution = n_queens(n, sampler)
+        self.assertEqual(len(solution), n)
         self.assertTrue(is_valid_solution(n, solution))
-
-    def test_10_queens(self):
-        n = 10
-
-        tries = 0
-        valid = False
-
-        while not valid and tries < 3:
-            solution = n_queens(n)
-            valid = is_valid_solution(n, solution)
-            tries += 1
-
-        self.assertTrue(valid)
 
 if __name__ == '__main__':
     unittest.main()
